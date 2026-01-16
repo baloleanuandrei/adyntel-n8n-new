369a50d0e4f38f4f3dc4547795c6d0e321dadf2f
Andrei Baloleanu <baloleanu.andrei@gmail.com>
2026-01-16 17:50:48 +0100

    Update credentials file formatting and test configuration

--- credentials/AdyntelApi.credentials.ts
+++ credentials/AdyntelApi.credentials.ts
@@ -72,16 +72,16 @@
 // }
 import {
 	IAuthenticateGeneric,
+	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
-	ICredentialTestRequest,
 } from 'n8n-workflow';
 
 export class AdyntelApi implements ICredentialType {
 	name = 'adyntelApi';
 	displayName = 'Adyntel API';
-	// eslint-disable-next-line n8n-nodes-base/cred-class-field-documentation-url-miscased
 	documentationUrl = 'https://docs.adyntel.com';
+
 	properties: INodeProperties[] = [
 		{
 			displayName: 'API Key',
@@ -98,22 +98,22 @@ export class AdyntelApi implements ICredentialType {
 			default: '',
 		},
 	];
+
 	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
 			body: {
-				'api_key': '={{$credentials.apiKey}}',
-				'email': '={{$credentials.email}}'
-			}
+				api_key: '={{$credentials.apiKey}}',
+				email: '={{$credentials.email}}',
+			},
 		},
 	};
+
 	test: ICredentialTestRequest = {
 		request: {
 			method: 'POST',
 			url: 'https://api.adyntel.com/facebook',
 			body: {
-				api_key: '={{$credentials.apiKey}}',
-				email: '={{$credentials.email}}',
 				company_domain: 'lokalise.com',
 			},
 		},
